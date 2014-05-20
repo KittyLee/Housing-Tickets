70fb85b4ddc851eac3624b14708c013d96d26601
KittyLee <katherine@codeforprogress.org>
2014-05-20 15:48:59 -0400

    added new template and modified views.py

--- app/views.py
+++ app/views.py
@@ -3,6 +3,11 @@
 from app import app
 from flask import render_template
 
+@app.route('/')
+def index():
+	return render_template("RHMHome.html")
+
+
 @app.route('/feedback')
 def feedback():
 	return render_template("RHMFeedback.html")
